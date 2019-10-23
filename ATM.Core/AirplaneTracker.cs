4cb93a06e17668a16355dbf1184843749ae58ee1
bvoelund@hotmail.com <bvoelund@hotmail.com>
2019-10-23 18:23:51 +0200

    Added AirplaneTracker

---
+++ ATM.Core/AirplaneTracker.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ATM.Core
+{
+    public class AirplaneTracker
+    {
+
+        public string Tag { get; set; }
+
+        public double Xcoordinate { get; set; }
+
+        public double Ycoordinate { get; set; }
+
+        public double altitude { get; set; }
+
+        public DateTime Timestamp { get; set; }
+    }
+}
