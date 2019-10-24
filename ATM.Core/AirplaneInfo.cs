432fc8216f2cf136e1b62a97dbf214c7151704b6
bvoelund@hotmail.com <bvoelund@hotmail.com>
2019-10-24 23:12:53 +0200

    Ændret klassenavne og tilføjet interface

--- ATM.Core/AirplaneInfo.cs
+++ ATM.Core/AirplaneInfo.cs
@@ -1,19 +1,20 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using ATM.Core.Interfaces
 
 namespace ATM.Core
 {
-    public class AirplaneTracker
+    public class AirplaneInfo : IAirplaneInfo
+    
     {
-
         public string Tag { get; set; }
 
         public double Xcoordinate { get; set; }
 
         public double Ycoordinate { get; set; }
 
-        public double altitude { get; set; }
+        public double Altitude { get; set; }
 
         public DateTime Timestamp { get; set; }
     }
---
+++ ATM.Core/Interfaces/IAirplaneInfo.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ATM.Core.Interfaces
+{
+    public interface IAirplaneInfo
+    {
+        string Tag { get; set; }
+
+        double Xcoordinate { get; set; }
+
+        double Ycoordinate { get; set; }
+
+        double Altitude { get; set; }
+
+        DateTime Timestamp { get; set; }
+    }
+
+}
