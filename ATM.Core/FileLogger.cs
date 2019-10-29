8097d8333975468924ebe977b63cfc520350465c
Anonym <201710692@post.au.dk>
2019-10-29 20:48:37 +0100

    Implemented filelogger - Umut

--- ATM.Core/FileLogger.cs
+++ ATM.Core/FileLogger.cs
@@ -1,13 +1,34 @@
 ﻿using System;
+using System.IO;
 using System.Collections.Generic;
 using System.Text;
 using ATM.Core.Interfaces;
 
 namespace ATM.Core
 {
-    public class FileLogger
+    public class FileLogger : ILog
     {
+        private string logpath;
 
+        public FileLogger(string path)
+        {
+            logpath = path;
+        }
+
+        public void Logs(LOGTYPE type, List<string> logMessages)
+        {
+            //Kun collisions
+            if (type != LOGTYPE.COLLISIONS) return;
+
+            StreamWriter sw = File.AppendText(logpath);
+
+            for (int i = 0; i < logMessages.Count; i++)
+            {
+                sw.WriteLine(logMessages[i]);
+            }
+
+            sw.Close();
+        }
 
     }
 }
