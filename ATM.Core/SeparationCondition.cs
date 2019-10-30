e17d5b706d38f6fc7450addc3d2a6b93c2b5a8c8
Plexandie <sebastianmik@hotmail.com>
2019-10-30 16:56:32 +0100

    Made the class SeparationCondition - Sebastian

--- ATM.Core/SeparationCondition.cs
+++ ATM.Core/SeparationCondition.cs
@@ -5,7 +5,32 @@ using ATM.Core.Interfaces;
 
 namespace ATM.Core
 {
-    class SeparationCondition
+    public class SeparationCondition : ITime
     {
+        private DateTime _time;
+        private Tuple<IAirplaneinfo, IAirplaneinfo> _pairAirplanes;
+        private bool _isLogged;
+
+        public Datetime Time
+        {
+            get => _time;
+        }
+
+        public Tuple<IAirplaneinfo, IAirplaneinfo> PairAirplanes
+        {
+            get => _pairAirplanes;
+        }
+
+        public bool IsLogged
+        {
+            get => _isLogged;
+            set => _isLogged = value;
+        }
+
+        public SeparationCondition(DateTime time, Tuple<IAirplaneinfo, IAirplaneinfo> pairAirplanes)
+        {
+            _time = time;
+            _pairAirplanes = pairAirplanes;
+        }
     }
 }
--- atmframework-swtgrp9/SeparationCondition.cs
+++ atmframework-swtgrp9/SeparationCondition.cs
@@ -5,7 +5,45 @@ using atmframework_swtgrp9.Interfaces;
 
 namespace atmframework_swtgrp9
 {
-    class SeparationCondition
+    public class SeparationCondition : ITime
     {
+        private DateTime _time;
+        private Tuple<IAirplaneinfo, IAirplaneinfo> _pairAirplanes;
+        private bool _isLogged;
+
+        public Datetime Time
+        {
+            get => _time;
+        }
+
+        public Tuple<IAirplaneinfo, IAirplaneinfo> PairAirplanes
+        {
+            get => _pairAirplanes;
+        }
+
+        public bool IsLogged
+        {
+            get => _isLogged;
+            set => _isLogged = value;
+        }
+
+        public SeparationCondition(DateTime time, Tuple<IAirplaneinfo, IAirplaneinfo> pairAirplanes)
+        {
+            _time = time;
+            _pairAirplanes = pairAirplanes;
+        }
+
+        //Bruges til at tjekke om en condition tidligere er forefundet.
+        public bool EqualCondition(SeparationCondition otherCondition)
+        {
+            if (string.EqualCondition(this.PairAirplanes.Item1.Tag, otherCondition.PairAirplanes.Item1.Tag) && string.EqualCondition(this.PairAirplanes.Item2.Tag, otherCondition.PairAirplanes.Item2.Tag))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
     }
 }
