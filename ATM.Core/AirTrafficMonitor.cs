8fb234fd0888625db0989521dd4607b29ccbc7c5
Anonym <201710692@post.au.dk>
2019-10-30 12:42:11 +0100

    More work on atm class, problem with transponder receiver

--- ATM.Core/AirTrafficMonitor.cs
+++ ATM.Core/AirTrafficMonitor.cs
@@ -1,15 +1,36 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using TransponderReceiver;
 using ATM.Core.Interfaces;
 
 namespace ATM.Core
 {
     class AirTrafficMonitor
     {
-        private RegisterCondition
+        private RegisterCondition _condition;
         private IAirspace<IAirplaneInfo> _airspace;
         private IAirplaneGenerator _generator;
-        
+        private ILog _consoleLog;
+        private ILog _fileLog;
+
+        public AirTrafficMonitor
+        (
+            FileLogger fileLog,
+            ConsoleLogger consoleLog,
+            RegisterCondition register,
+            IAirspace<IAirplaneInfo> airspace,
+            IAirplaneGenerator generator
+        )
+        {
+            _fileLog = fileLog;
+            _consoleLog = consoleLog;
+            _condition = register;
+            _airspace = airspace;
+            _generator = generator;
+        }
+
+        public  void OnEvent
+
     }
 }
