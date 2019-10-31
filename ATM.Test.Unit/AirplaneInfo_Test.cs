4c2302feefb0704ebc17420c38a95af29626a11f
bvoelund@hotmail.com <bvoelund@hotmail.com>
2019-10-31 10:53:43 +0100

    Testdata added for AirplaneInfo testcase

--- ATM.Test.Unit/AirplaneInfo_Test.cs
+++ ATM.Test.Unit/AirplaneInfo_Test.cs
@@ -1,12 +1,54 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using atmframework_swtgrp9;
+using NSubstitute;
+using NUnit.Framework;
 
 namespace ATM.Test.Unit
 {
+    [TestFixture]
     class AirplaneInfo_Test
     {
+        internal class ClassTestData
+        {
+            //Arrange 
+
+            private static AirplaneInfo testAirplane = new AirplaneInfo();
+            private static int testInteger = 100;
+            private static double testDouble = 100.0;
+            private static string testString = "testString";
+            private static DateTime testTime = new DateTime(2019, 31, 10);
+
+
+
+            //Act
+            public static IEnumerable TestProperties
+            {
+                //Inserting data for the AirplaneInfo variables
+                get
+                {
+                    //Act
+                    yield return new TestCaseData(new AirplaneInfo().Tag = testString, "testString").SetName("Plane Tag");
+
+                    yield return new TestCaseData(new AirplaneInfo().Altitude = testInteger, 100).SetName("Plane Altitude");
+
+                    yield return new TestCaseData(new AirplaneInfo().X = testInteger, 100).SetName("Plane X-coordinate");
+
+                    yield return new TestCaseData(new AirplaneInfo().Y = testInteger, 100).SetName("Plane Y-coordinate");
+
+                    yield return new TestCaseData(new AirplaneInfo().Course = testDouble, 100.0).SetName("Plane Course");
+
+                    yield return new TestCaseData(new AirplaneInfo().Velocity = testDouble, 100.0).SetName("Plane Velocity");
+
+                    yield return new TestCaseData(new AirplaneInfo().TimeStamp = testTime, new DateTime(2019, 31, 10)).SetName("Plane Time");
+
+
+                }
+            }
+        }
     }
 }
