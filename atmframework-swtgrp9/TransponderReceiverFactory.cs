b7f0839bbec70dd8b11e3d15e970a12c75fc9390
Anonym <201710692@post.au.dk>
2019-10-31 10:50:19 +0100

    added dotcovercoverageconfig to projekt. also added transponder receiver files but might need change

---
+++ atmframework-swtgrp9/TransponderReceiverFactory.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace atmframework_swtgrp9
+{
+    public class TransponderReceiverFactory
+    {
+        public static ITransponderReceiver CreateTransponderDataReceiver();
+    }
+}
