9bfac5ec4569e46982b323d343bcd44fb7089bbf
Plexandie <sebastianmik@hotmail.com>
2019-11-01 13:56:26 +0100

    Started on tests for collisiondetector

--- ATM.Test.Unit/CollisionDetector_Test.cs
+++ ATM.Test.Unit/CollisionDetector_Test.cs
@@ -1,12 +1,42 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using atmframework_swtgrp9;
+using atmframework_swtgrp9.Interfaces;
+using NSubstitute;
+using NUnit.Framework;
 
 namespace ATM.Test.Unit
 {
-    class CollisionDetector_Test
+    [TestFixture]
+    class CollisionDetectorTest
     {
+        private List<IAirplaneInfo> _testAirspace;
+        private IAirplaneInfo _testAirplaneInfo1;
+        private IAirplaneInfo _testAirplaneInfo2;
+
+        private ILog _log;
+
+        private CollisionDetector _uut;
+
+        [SetUp]
+        public void Setup()
+        {
+            _log = Substitute.For<ILog>();
+
+            _uut = new CollisionDetector(_log);
+
+            _testAirspace = new List<IAirplaneInfo>();
+            _testAirplaneInfo1 = Substitute.For<IAirplaneInfo>();
+            _testAirplaneInfo2 = Substitute.For<IAirplaneInfo>();
+
+            _testAirspace.Add(_testAirplaneInfo1);
+            _testAirspace.Add(_testAirplaneInfo2);
+
+
+        }
     }
 }
