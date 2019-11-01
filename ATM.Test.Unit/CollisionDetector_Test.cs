e3ca9e121fe1c7029e2f099451eebf104c42d151
Plexandie <sebastianmik@hotmail.com>
2019-11-01 18:50:14 +0100

    Made test to test if Logs function gets called when collision happens - seb

--- ATM.Test.Unit/CollisionDetector_Test.cs
+++ ATM.Test.Unit/CollisionDetector_Test.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Collections;
+using System.ComponentModel.Design;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -69,5 +70,21 @@ namespace ATM.Test.Unit
 
         //    Assert.That(_uut.GetConditions().Count, Is.EqualTo(0));
         //}
+
+        [Test] //Test for om der Log funktionen bliver kaldt når der er collision
+        public void CheckIfLogCalled()
+        {
+            _testAirplaneInfo1.X.Returns(35000);
+            _testAirplaneInfo1.Y.Returns(35000);
+            _testAirplaneInfo1.Altitude.Returns(600);
+
+            _testAirplaneInfo2.X.Returns(35000);
+            _testAirplaneInfo2.Y.Returns(35000);
+            _testAirplaneInfo2.Altitude.Returns(1100);
+
+            _uut.Register(_testAirspace);
+
+            _log.Received().Logs(LOGTYPE.COLLISIONS,  Arg.Any<List<string>>());
+        }
     }
 }
