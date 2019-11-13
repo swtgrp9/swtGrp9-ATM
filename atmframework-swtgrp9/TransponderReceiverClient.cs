fc14f9eba68b4c3fa14b14fc4896283948d06db7
Plexandie <sebastianmik@hotmail.com>
2019-11-13 17:57:27 +0100

    Included TransponderReceiverClient from handout(FRABJ) and made changes to main. TRC and main needs more changes

---
+++ atmframework-swtgrp9/TransponderReceiverClient.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using atmframework_swtgrp9;
+using atmframework_swtgrp9.Interfaces;
+using TransponderReceiver;
+
+namespace atmframework_swtgrp9
+{
+    class TransponderReceiverClient
+    {
+        private ITransponderReceiver receiver;
+
+        // Using constructor injection for dependency/ies
+        public TransponderReceiverClient(ITransponderReceiver receiver)
+        {
+            // This will store the real or the fake transponder data receiver
+            this.receiver = receiver;
+
+            // Attach to the event of the real or the fake TDR
+            this.receiver.TransponderDataReady += ReceiverOnTransponderDataReady;
+        }
+
+        private void ReceiverOnTransponderDataReady(object sender, RawTransponderDataEventArgs e)
+        {
+            // Just display data
+            foreach (var data in e.TransponderData)
+            {
+                System.Console.WriteLine($"Transponderdata {data}");
+            }
+        }
+    }
+}
